ab0ccadaaa6a1a7c0ea49f0c54a602f4993d19ae
Kamila <144148604+ulevick@users.noreply.github.com>
2023-11-04 17:38:43 +0200

    Update Bullet.cs

--- BerzerkGame/Bullet.cs
+++ BerzerkGame/Bullet.cs
@@ -41,6 +41,36 @@ namespace BarzerkGame
             return true;
         }
 
+        public bool Move(char[,] map)
+        {
+            int newX = X, newY = Y;
+
+            switch (direction)
+            {
+                case Direction.UP:
+                    newY--;
+                    break;
+                case Direction.DOWN:
+                    newY++;
+                    break;
+                case Direction.RIGHT:
+                    newX++;
+                    break;
+                case Direction.LEFT:
+                    newX--;
+                    break;
+            }
+
+            if (IsMoveValid(newX, newY, map))
+            {
+                X = newX;
+                Y = newY;
+                return false;
+            }
+
+            return true;
+        }
+
         public override void Draw(bool clear = false)
         {
             Console.SetCursorPosition(X, Y + 2); // Y + 2 because of some offset handling, which could be specific to the game's UI.
